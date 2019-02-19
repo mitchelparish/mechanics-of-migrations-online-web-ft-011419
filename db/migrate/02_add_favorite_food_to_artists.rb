685bf5bdfd097083f336a141678e3c765e81ef15
Mitchel Parish <mitchelparish@gmail.com>
2019-02-19 17:04:07 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,5 +1,5 @@
 class AddFavoriteFoodToArtists < ActiveRecord::Migration
   def change
-    add_column :artists, :favorite_food, :string
-  end
+    add_column :artists, :favorite_food, :string  # Add column to artists table, called
+  end                                             # favorite_food, and set the datatype to a string.
 end
